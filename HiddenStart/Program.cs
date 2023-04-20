d62a9ffad579aca4b0051c3d7e4fa43a2dde4ae8
ilyabratskih <ilyabratskih@mail.ru>
2023-04-21 00:45:00 +0300

    write project without git

---
+++ HiddenStart/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace HiddenStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = Environment.CurrentDirectory + "/" + name;
+            psi.UseShellExecute = false;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            psi.CreateNoWindow = true;
+            Process.Start(psi);
+        }
+    }
+}
